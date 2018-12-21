7eefaefee701bccc3bd931a11153b99ddeb33a1b
Matt <mattlam@uw.edu>
2018-12-21 15:03:18 -0800

    Don't message if ping result is success. Only message on failure.

--- src/components/pinger/pinger.php
+++ src/components/pinger/pinger.php
@@ -30,4 +30,6 @@ $pinger = Pinger::getInstance($responseLogger);
 
 // Ping URLs and log results
 $pingResult = $pinger->pingUrls($urlsArray);
-echo ($pingResult > 0) ? "Success\n" : "Failure\n";
+if ($pingResult < 1) {
+    echo "Failure\n";
+}
